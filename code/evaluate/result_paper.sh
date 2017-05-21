b273e420fdde53293081d620b5d2e8bef005a1ee
uzsEzrmS <liangliangsx@gmail.com>
2017-05-21 11:19:48 +0800

    modify the scripts for line 1

--- code/evaluate/result_paper.sh
+++ code/evaluate/result_paper.sh
@@ -1,4 +1,3 @@
-#python get_result_paper.py /home/liangliang/workspace/digg/code/result/KG2EEL_random_100_time 200 & ^M
 python get_result_paper.py TransE_random_100_time 200 &
 python get_result_paper.py DPGE_random_100_time 200 &
 python get_result_paper.py KG2EEL_random_100_time 200 &
